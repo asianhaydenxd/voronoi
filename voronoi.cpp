2b31944013732a9bf2197f4d369f31b758ac73ef
Hayden <asianhaydenxd@gmail.com>
2022-06-08 08:56:35 -0400

    Create manhattan distance function

--- voronoi.cpp
+++ voronoi.cpp
@@ -3,6 +3,10 @@
 #include<cmath>
 #include<ctime>
 
+float manhattanDistance(int x1, int y1, int x2, int y2) {
+    return abs(x1 - x2) + abs(y1 - y2);
+}
+
 float euclidDistance(int x1, int y1, int x2, int y2) {
     return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
