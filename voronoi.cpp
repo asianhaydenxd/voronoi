c2ad8fb8ab88b3f12878ac5954116cd6c1d8f668
Hayden <asianhaydenxd@gmail.com>
2022-06-08 10:49:25 -0400

    Plot all points rather than first 4

--- voronoi.cpp
+++ voronoi.cpp
@@ -24,36 +24,33 @@ float euclideanDistance(int x1, int y1, int x2, int y2) {
 void drawVoronoi(DistanceMode mode, int width, int height, int numOfPoints, Point points[], const char characters[]) {
     for (int iy = 0; iy < height; iy++) {
         for (int ix = 0; ix < width; ix++) {
-            if (points[0].x == ix && points[0].y == iy) {
-                std::cout << "()";
-            } else if (points[1].x == ix && points[1].y == iy) {
-                std::cout << "()";
-            } else if (points[2].x == ix && points[2].y == iy) {
-                std::cout << "()";
-            } else if (points[3].x == ix && points[3].y == iy) {
-                std::cout << "()";
-            } else {
-
-                float distances[numOfPoints];
-
-                for (int i = 0; i < numOfPoints; i++) {
-                    if (mode == Euclidian) distances[i] = euclideanDistance(ix, iy, points[i].x, points[i].y);
-                    if (mode == Manhattan) distances[i] = manhattanDistance(ix, iy, points[i].x, points[i].y);
+            for (int i = 0; i < numOfPoints; i++) {
+                if (points[i].x == ix && points[i].y == iy) {
+                    std::cout << "()";
+                    break;
                 }
+                if (i + 1 == numOfPoints) {
+                    float distances[numOfPoints];
 
-                float min = distances[0];
-
-                for (int i = 0; i < numOfPoints; i++) {
-                    if (distances[i] < min) {
-                        min = distances[i];
+                    for (int i = 0; i < numOfPoints; i++) {
+                        if (mode == Euclidian) distances[i] = euclideanDistance(ix, iy, points[i].x, points[i].y);
+                        if (mode == Manhattan) distances[i] = manhattanDistance(ix, iy, points[i].x, points[i].y);
                     }
-                }
 
-                for (int i = 0; i < numOfPoints; i++) {
-                    if (min == distances[i]) {
-                        char character = characters[static_cast<int>(std::floor(69 * i / numOfPoints) + 0.5)];
-                        std::cout << character << character;
-                        break;
+                    float min = distances[0];
+
+                    for (int i = 0; i < numOfPoints; i++) {
+                        if (distances[i] < min) {
+                            min = distances[i];
+                        }
+                    }
+
+                    for (int i = 0; i < numOfPoints; i++) {
+                        if (min == distances[i]) {
+                            char character = characters[static_cast<int>(std::floor(69 * i / numOfPoints) + 0.5)];
+                            std::cout << character << character;
+                            break;
+                        }
                     }
                 }
             }
